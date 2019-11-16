8793f12f686f8e254371742a764fb1d0f0e4efd0
matthiasjk5 <matthias.jk5@gmail.com>
2019-11-16 13:17:38 -0500

    added ShefferStroke

--- ShefferStroke/Program.cs
+++ ShefferStroke/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//The NAND operation(also known as a Sheffer stroke) is an operation on two logical values.It produces true, if - and only if - at least one of the operands is false.
+//Given two boolean values a and b, return the value of A NAND B.
+
+using System;
 
 namespace ShefferStroke
 {
