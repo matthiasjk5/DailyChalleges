957582eb439f2bbf25824d7355aeb9aa87cd5e7f
matthiasjk5 <matthias.jk5@gmail.com>
2019-12-18 14:44:21 -0500

    String Reverse

---
+++ StringReverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringReverse("Hello"));
+        }
+
+        static string StringReverse(string str)
+        {
+            char[] chars = new char[str.Length];
+            char[] arr = new char[200];
+            int index = 0;
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                chars[index++] = str[i];
+            }
+            return new string(chars);
+        }
+    }
+}
---
+++ squareDigitsSequence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace squareDigitsSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int SquareDigitsSequence(int a0)
+        {
+            var digits = a0.ToString().ToCharArray();
+
+            
+        }
+    }
+}
